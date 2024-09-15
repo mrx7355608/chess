c5cf5a7e6d4f87fb3c4d41be2b615f5fa87ad6db
mrx7355608 <mrx7355608@gmail.com>
2024-09-16 01:42:43 +0500

    updated mouseReleased() logic

--- src/chess/ui/MouseEventHandler.java
+++ src/chess/ui/MouseEventHandler.java
@@ -4,6 +4,7 @@
  */
 package chess.ui;
 
+import chess.core.Move;
 import chess.core.Piece;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -61,13 +62,23 @@ public class MouseEventHandler extends MouseAdapter {
     public void mouseReleased(MouseEvent me) {
         this.toRow = me.getY() / 60;
         this.toCol = me.getX() / 60;
-        this.board.makeMove(fromRow, fromCol, toRow, toCol, piece);
+        
+        Move move = new Move(toRow, toCol, piece);
 
         if (this.piece != null) {
-            this.piece.setX(toCol * 60);
-            this.piece.setY(toRow * 60);
+            // If move is valid, then update the board and change X and Y positions
+            // of the piece along with its row and column
+            if (this.board.isValidMove(move)) {
+                this.board.makeMove(fromRow, fromCol, toRow, toCol, piece);
+                this.piece.setX(toCol * 60);
+                this.piece.setY(toRow * 60);
+                this.piece.updatePiece(toRow, toCol);
+            } else {
+                // Otherwise, move the piece back to it's original position
+                this.piece.setX(fromCol * 60);
+                this.piece.setY(fromRow * 60);
+            }
             this.board.clearMoves();
-            this.piece.updatePiece(toRow, toCol);
             this.board.repaint();
         }
 
