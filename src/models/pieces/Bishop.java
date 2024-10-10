66eb4f2c2cc9eebfa8543b3cb0193d843662ecf1
mrx7355608 <mrx7355608@gmail.com>
2024-10-10 17:49:37 +0500

    using recursion to find bishop moves

--- src/models/pieces/Bishop.java
+++ src/models/pieces/Bishop.java
@@ -21,103 +21,60 @@ public class Bishop extends Piece {
     @Override
     public ArrayList<Move> findMoves(Piece[][] board) {
         ArrayList<Move> moves = new ArrayList();
-
-        // Make moves for upper left diagonal
-        int nextCol = this.col;
-        for (int i = this.row - 1; i >= 0; i--) {
-            nextCol -= 1;
-
-            // 1. If move goes out of board, ignore it
-            if (this.isOutsideTheBoard(nextCol)) {
-                continue;
-            }
-
-            // 2. If there is no piece at this new position, add it in moves
-            Piece p = board[i][nextCol];
-            if (p == null) {
-                moves.add(new Move(i, nextCol, this));
-                continue;
-
-                // 3. If their is a piece at this new position and it is an
-                // enemy piece, then add it in move and break this loop,because
-                // on encountering an enemy piece you can only make move uptil
-                // that piece (capture) and cannot go further than that
-            } else if (this.isEnemyPiece(board, i, nextCol)) {
-                moves.add(new Move(i, nextCol, this));
-                break;
-            }
-
-            // 4. Now, at this line of code, if the tile is neither empty, nor
-            // it contains enemy piece, there's only one possibility that, it contains
-            // ally piece i.e the piece having same color as current piece. So, 
-            // break the loop as, we cannot proceed further according to chess
-            // rules
-            break;
-        }
-
-        // Make moves for upper right diagonal
-        nextCol = this.col;
-        for (int i = this.row - 1; i >= 0; i--) {
-            nextCol += 1;
-
-            if (this.isOutsideTheBoard(nextCol)) {
-                continue;
-            }
-
-            Piece p = board[i][nextCol];
-            if (p == null) {
-                moves.add(new Move(i, nextCol, this));
-                continue;
-            } else if (this.isEnemyPiece(board, i, nextCol)) {
-                System.out.println("Enemy");
-                moves.add(new Move(i, nextCol, this));
-                break;
-            }
-
-            break;
-        }
-
-        // Make moves for lower left diagonal
-        nextCol = this.col;
-        for (int i = this.row + 1; i < 8; i++) {
-            nextCol -= 1;
-            if (this.isOutsideTheBoard(nextCol)) {
-                continue;
-            }
-
-            Piece p = board[i][nextCol];
-            if (p == null) {
-                moves.add(new Move(i, nextCol, this));
-                continue;
-            } else if (this.isEnemyPiece(board, i, nextCol)) {
-                moves.add(new Move(i, nextCol, this));
-                break;
-            }
-
-            break;
-        }
-
-        // Make moves for lower right diagonal
-        nextCol = this.col;
-        for (int i = this.row + 1; i < 8; i++) {
-            nextCol += 1;
-
-            if (this.isOutsideTheBoard(nextCol)) {
-                continue;
-            }
-
-            Piece p = board[i][nextCol];
-            if (p == null) {
-                moves.add(new Move(i, nextCol, this));
-                continue;
-            } else if (this.isEnemyPiece(board, i, nextCol)) {
-                moves.add(new Move(i, nextCol, this));
-                break;
-            }
-
-            break;
-        }
+        
+        this.calc(this.col, this.row, board, moves, "upper-left");
+        this.calc(this.col, this.row, board, moves, "upper-right");
+        this.calc(this.col, this.row, board, moves, "lower-left");
+        this.calc(this.col, this.row, board, moves, "lower-right");
 
         return moves;
     }
+
+    public void calc(int col, int row, Piece[][] board, ArrayList<Move> moves, String direction) {
+        // Create new coordinates (row,col) according to direction of the diagonal
+        switch (direction) {
+            case "upper-left" -> {
+                row -= 1;
+                col -= 1;
+            }
+                
+            case "upper-right" -> {
+                row -= 1;
+                col += 1;
+            }
+
+            case "lower-left" -> {
+                row += 1;
+                col -= 1;
+            }
+                
+            case "lower-right" -> {
+                row += 1;
+                col += 1;
+            }
+                
+            default -> {
+                row -= 1;
+                col -= 1;
+            }
+        }
+        
+        // 1. If either row or col are outside the board, end recursion
+        if (this.isOutsideTheBoard(col) || this.isOutsideTheBoard(row)) {
+            return;
+        }
+
+        // 2. If the tile is empty then, add those coordinates in moves.
+        Piece p = board[row][col];
+        if (p == null) {
+            moves.add(new Move(row, col, this));
+            
+            // Find other possible moves recursively
+            this.calc(col, row, board, moves, direction);
+            
+        } else if (this.isEnemyPiece(board, row, col)) {
+            moves.add(new Move(row, col, this));
+        }
+    }
+
 }
