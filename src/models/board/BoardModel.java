2154798618e3583421b645dd751daf86f582629b
mrx7355608 <mrx7355608@gmail.com>
2024-10-11 17:55:58 +0500

    created separate arrays for storing kings positions and pieces

--- src/models/board/BoardModel.java
+++ src/models/board/BoardModel.java
@@ -15,6 +15,13 @@ public class BoardModel {
 
     private static final Piece[][] board = new Piece[8][8];
 
+    private int[] whiteKingPosition = new int[2]; // [row, col] for white king
+    private int[] blackKingPosition = new int[2]; // [row, col] for black king
+    private boolean isBlackInCheck = false;
+    private boolean isWhiteInCheck = false;
+    private ArrayList<Piece> whitePieces = new ArrayList();
+    private ArrayList<Piece> blackPieces = new ArrayList();
+
     public BoardModel() {
         try {
             this.setupBoard();
@@ -24,44 +31,129 @@ public class BoardModel {
     }
 
     private void setupBoard() throws IOException {
-        board[0][0] = new Rook(0, 0, "black");
-        board[0][1] = new Knight(0, 1, "black");
-        board[0][2] = new Bishop(0, 2, "black");
-        board[0][3] = new Queen(0, 3, "black");
-        board[0][4] = new King(0, 4, "black");
-        board[0][5] = new Bishop(0, 5, "black");
-        board[0][6] = new Knight(0, 6, "black");
-        board[0][7] = new Rook(0, 7, "black");
+        // Create pieces
+        Piece blackRook1 = new Rook(0, 0, "black");
+        Piece blackKnight1 = new Knight(0, 1, "black");
+        Piece blackBishop1 = new Bishop(0, 2, "black");
+        Piece blackQueen = new Queen(0, 3, "black");
+        Piece blackKing = new King(0, 4, "black");
+        Piece blackBishop2 = new Bishop(0, 5, "black");
+        Piece blackKnight2 = new Knight(0, 6, "black");
+        Piece blackRook2 = new Rook(0, 7, "black");
 
-        board[7][0] = new Rook(7, 0, "white");
-        board[7][1] = new Knight(7, 1, "white");
-        board[7][2] = new Bishop(7, 2, "white");
-        board[7][3] = new Queen(7, 3, "white");
-        board[7][4] = new King(7, 4, "white");
-        board[7][5] = new Bishop(7, 5, "white");
-        board[7][6] = new Knight(7, 6, "white");
-        board[7][7] = new Rook(7, 7, "white");
+        Piece whiteRook1 = new Rook(7, 0, "white");
+        Piece whiteKnight1 = new Knight(7, 1, "white");
+        Piece whiteBishop1 = new Bishop(7, 2, "white");
+        Piece whiteQueen = new Queen(7, 3, "white");
+        Piece whiteKing = new King(7, 4, "white");
+        Piece whiteBishop2 = new Bishop(7, 5, "white");
+        Piece whiteKnight2 = new Knight(7, 6, "white");
+        Piece whiteRook2 = new Rook(7, 7, "white");
+
+        // Set pieces on board
+        board[0][0] = blackRook1;
+        board[0][1] = blackKnight1;
+        board[0][2] = blackBishop1;
+        board[0][3] = blackQueen;
+        board[0][4] = blackKing;
+        board[0][5] = blackBishop2;
+        board[0][6] = blackKnight2;
+        board[0][7] = blackRook2;
+
+        board[7][0] = whiteRook1;
+        board[7][1] = whiteKnight1;
+        board[7][2] = whiteBishop1;
+        board[7][3] = whiteQueen;
+        board[7][4] = whiteKing;
+        board[7][5] = whiteBishop2;
+        board[7][6] = whiteKnight2;
+        board[7][7] = whiteRook2;
+
+        // Store pieces in their specific arrays so that it's easy
+        // to retrieve them in detectCheck() method logic
+        blackPieces.add(blackRook2);
+        blackPieces.add(blackRook2);
+        blackPieces.add(blackRook2);
+        blackPieces.add(blackRook2);
+        blackPieces.add(blackRook2);
+        blackPieces.add(blackRook2);
+        blackPieces.add(blackRook2);
+        blackPieces.add(blackRook2);
+
+        whitePieces.add(whiteRook1);
+        whitePieces.add(whiteKnight1);
+        whitePieces.add(whiteBishop1);
+        whitePieces.add(whiteQueen);
+        whitePieces.add(whiteKing);
+        whitePieces.add(whiteBishop2);
+        whitePieces.add(whiteKnight2);
+        whitePieces.add(whiteRook2);
+
+        // Store black king's initial position
+        blackKingPosition[0] = 0;
+        blackKingPosition[1] = 4;
+
+        // Store white king's initial position
+        whiteKingPosition[0] = 7;
+        whiteKingPosition[1] = 4;
 
         // Add pawns
-        for (int i = 1; i < 7; i++) {
-            for (int j = 0; j < 8; j++) {
-                switch (i) {
-                    case 1 -> {
-                        board[i][j] = new Pawn(i, j, "black");
+        for (int row = 1; row < 7; row++) {
+
+            // Using switch case to add pawns on specific rows, according to
+            // how they are arranged on chess board
+            switch (row) {
+                case 1 -> {
+                    for (int col = 0; col < 8; col++) {
+                        Piece pawn = new Pawn(row, col, "black");
+                        board[row][col] = pawn;
+                        blackPieces.add(pawn);
                     }
-                    case 6 -> {
-                        board[i][j] = new Pawn(i, j, "white");
-                    }
-                    default ->
-                        board[i][j] = null;
                 }
+
+                case 6 -> {
+                    for (int col = 0; col < 8; col++) {
+                        Piece pawn = new Pawn(row, col, "white");
+                        board[row][col] = pawn;
+                        whitePieces.add(pawn);
+                    }
+                }
+
+                default -> {
+                    for (int col = 0; col < 8; col++) {
+                        board[row][col] = null;
+                    }
+                }
+
             }
+
         }
     }
 
     public void makeMove(int fromRow, int fromCol, int toRow, int toCol, Piece piece) {
         board[toRow][toCol] = piece;
         board[fromRow][fromCol] = null;
+
+        // Remove captured piece from its array
+        Piece capturedPiece = board[fromRow][fromCol];
+        if (capturedPiece != null) {
+            if (capturedPiece.color.equals("white")) {
+                whitePieces.remove(capturedPiece);
+            } else {
+                blackPieces.remove(capturedPiece);
+            }
+        }
+
+        // If the piece is a king, update the corresponding king's position
+        if (piece instanceof King) {
+            if (piece.color.equals("white")) {
+                whiteKingPosition[0] = toRow;
+                whiteKingPosition[1] = toCol;
+            } else {
+                blackKingPosition[0] = toRow;
+                blackKingPosition[1] = toCol;
+            }
+        }
     }
 
     private boolean containsMove(Move move, ArrayList<Move> moves) {
@@ -105,4 +197,37 @@ public class BoardModel {
     public Piece getPiece(int row, int col) {
         return board[row][col];
     }
+
+    public int[] getWhiteKingPosition() {
+        return whiteKingPosition;
+    }
+
+    public int[] getBlackKingPosition() {
+        return blackKingPosition;
+    }
+
+    public void setIsBlackInCheck(boolean isBlackInCheck) {
+        this.isBlackInCheck = isBlackInCheck;
+    }
+
+    public void setIsWhiteInCheck(boolean isWhiteInCheck) {
+        this.isWhiteInCheck = isWhiteInCheck;
+    }
+
+    public boolean isBlackInCheck() {
+        return isBlackInCheck;
+    }
+
+    public boolean isWhiteInCheck() {
+        return isWhiteInCheck;
+    }
+
+    public ArrayList<Piece> getWhitePieces() {
+        return whitePieces;
+    }
+
+    public ArrayList<Piece> getBlackPieces() {
+        return blackPieces;
+    }
+
 }
